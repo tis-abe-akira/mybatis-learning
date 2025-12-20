07f3ecf6a723bcc3f3a2b519c2ae69d8ad237891
tis-abe-akira <abe.akira@tis.co.jp>
2025-12-20 20:11:34 +0900

    エンティティパッケージの情報を追加

---
+++ src/main/java/com/example/mybatislearning/entity/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * エンティティパッケージを提供します（2個のクラス）
+ *
+ * <p>
+ * 主なクラス:
+ * </p>
+ * <ul>
+ * <li>Organization</li>
+ * <li>Project</li>
+ * </ul>
+ *
+ * <p>
+ * このパッケージはMyBatisのエンティティに関連するクラスを提供します。
+ * </p>
+ */
+package com.example.mybatislearning.entity;
